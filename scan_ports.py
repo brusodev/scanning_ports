7da839ea268b9d5504325bc23a48feafa5118816
Bruno Vargas <brunosoaresvargas@gmail.com>
2024-12-01 22:22:41 -0300

    Adicionando o código

---
+++ scan_ports.py
@@ -0,0 +1,24 @@
+import socket
+
+def port_scanner(target, ports):
+    hostname = socket.gethostbyname(target)
+    print(f"Mapeando... {target} ({hostname})")
+
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = sock.connect_ex((hostname, port))
+
+        if result == 0:
+            print(f"Porta {port} está aberta")
+        sock.close()
+        if result != 0: 
+            print(f"Porta {port} está fechada")
+        
+    print("Scan finalizado")
+
+
+#Exemplo de uso
+target = "brsodev.myftp.org"
+ports = [21, 22, 80, 443, 8080]
+port_scanner(target, ports)
